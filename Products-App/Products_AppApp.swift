22345ed54f08b2fa9b15a704ec01d2d122bb6f60
Peter Murín <peter.murin@kios.sk>
2023-02-22 18:18:50 +0100

    Initial Commit

---
+++ Products-App/Products_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Products_AppApp.swift
+//  Products-App
+//
+//  Created by Peter Murin on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Products_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
